9864eedad5463df3c554d3860560a5682736fe2e
MrDaGree <dawsonmax10@gmail.com>
2020-10-19 17:43:32 -0500

    Added way for each module to have individual GUI interface for changing settings, prettied things as well

--- modules/logger.py
+++ modules/logger.py
@@ -4,6 +4,8 @@ class Logger:
 
     log = []
 
+    usePrintStatements = False
+
     def __init__(self):
         pass
 
@@ -11,21 +13,24 @@ class Logger:
         dateTimeObj = datetime.now()
         timestampStr = dateTimeObj.strftime("[%m-%d-%Y] [%H:%M:%S]")
 
-        print("LTKS: " + message)
+        if self.usePrintStatements:
+            print("LTKS: " + message)
         self.log.append(timestampStr + " LOG | " + message)
 
     def logError(self, message):
         dateTimeObj = datetime.now()
         timestampStr = dateTimeObj.strftime("[%m-%d-%Y] [%H:%M:%S]")
 
-        print("LTKS [ERROR]: " + message)
+        if self.usePrintStatements:
+            print("LTKS [ERROR]: " + message)
         self.log.append(timestampStr + " ERROR | " + message)
 
     def logAlert(self, message):
         dateTimeObj = datetime.now()
         timestampStr = dateTimeObj.strftime("[%m-%d-%Y] [%H:%M:%S]")
 
-        print("LTKS [ALERT]: " + message)
+        if self.usePrintStatements:
+            print("LTKS [ALERT]: " + message)
         self.log.append(timestampStr + " ALERT | " + message)
 
     def getLogs(self):
